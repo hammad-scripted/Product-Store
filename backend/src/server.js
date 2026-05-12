48018f363902ccfcface20886dba322c3b154e72
Mohammad Hammad <hammad.scripted@gmail.com>
2026-05-12 22:14:44 +0530

    project completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,11 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1', productRouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+  app.get('/*splat', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+  });
 }
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
-});
 connectDB()
   .then((connection) => {
     console.log(chalk.magenta('connected to database'));
